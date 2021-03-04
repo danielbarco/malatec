f661e79b441a61e4da8ed065a44cc2cd1d97e119
danielbarco <danielbarco@users.noreply.github.com>
2021-03-04 17:07:14 +0100

    working on slicing images

--- src/pipeline.py
+++ src/pipeline.py
@@ -12,6 +12,7 @@ from skimage.io import imread
 from skimage.transform import resize
 from tensorflow.keras.models import Model, load_model
 import tensorflow as tf
+import cv2
 
 
 def get_paths(PATH):
@@ -50,95 +51,70 @@ def get_imgs(PATH, IMG_HEIGHT = 256, IMG_WIDTH = 256, IMG_CHANNELS = 3):
         imgs[idx] = img
     return imgs, sizes_imgs    
 
-def slice_img(INPUT_FOLDER, RESIZE_FACTOR = 1,
-                  sliceHeight=256, sliceWidth=256,
-                  zero_frac_thresh=0, overlap=0, pad=0, verbose = False,
+def slice_img(INPUT_IMG, OUTPUT_DIR, RESIZE_FACTOR = 1,
+                  SLICE_HEIGHT = 256, SLICE_WIDTH = 256,
+                  ZERO_FRAC_TRESH = 0, OVERLAP = 0, PAD = 0, VERBOSE = False,
                  ):
     '''
-    ADAPTED FROM YOLT/SCRIPTS/SLICE_IM.PY
-    Assume input_im is rgb
-    Slice large image into smaller pieces,
-    ignore slices with a percentage null greater then zero_fract_thresh'''
-    if verbose == True:
-        print('############# slice_img #############')
-    img = cv2.imread(input_im, 1)  # color
-    if create_mask:
-        mask = cv2.imread(input_mask, 0)
-    category_num = [classes_dic[k] for k in category] 
+    slices image into smaller windows
+    INPUT_IMG,
+    OUTPUT_DIR, 
+    RESIZE_FACTOR = 1,
+    SLICE_HEIGHT = 256, 
+    SLICE_WIDTH = 256,
+    ZERO_FRAC_TRESH = 0, 
+    OVERLAP = 0, 
+    PAD = 0, 
+    VERBOSE = False,
+    '''
+    #adapted from https://github.com/CosmiQ/simrdwn/blob/9f91eac5d0769400f89eefc145d67f0ee209d8fc/simrdwn/core/slice_im.py
 
-    resized_img = cv2.resize(img, (int(round(img.shape[1] * resize_factor)), int(round(img.shape[0] * resize_factor))))
-    if create_mask == True:
-        resized_mask = cv2.resize(mask, (int(round(mask.shape[1] * resize_factor)), int(round(mask.shape[0] * resize_factor))))
-    resized_box_coords = np.array(np.round(np.array(box_coords) * np.array(resize_factor)).astype(int))
-
-
+    img = cv2.imread(INPUT_IMG, 1)  # color
+    resized_img = cv2.resize(img, (int(round(img.shape[1] * RESIZE_FACTOR)), int(round(img.shape[0] * RESIZE_FACTOR))))
     im_h, im_w = resized_img.shape[:2]
-    if verbose:
-        print('im_h', im_h)
-        print('im_w', im_w)
-
-    win_size = sliceHeight*sliceWidth
+    win_size = SLICE_HEIGHT * SLICE_WIDTH
 
     # if slice sizes are large than image, pad the edges
-    if sliceHeight > im_h:
-        pad = sliceHeight - im_h
-    if sliceWidth > im_w:
-        pad = max(pad, sliceWidth - im_w)
+    if SLICE_HEIGHT > im_h:
+        pad = SLICE_HEIGHT - im_h
+    if SLICE_WIDTH > im_w:
+        pad = max(pad, SLICE_WIDTH - im_w)
     # pad the edge of the image with black pixels
     if pad > 0:
         border_color = (0, 0, 0)
         resized_img = cv2.copyMakeBorder(resized_img, pad, pad, pad, pad,
                                    cv2.BORDER_CONSTANT, value=border_color)
-        if create_mask == True:
-            resized_mask = cv2.copyMakeBorder(resized_img, pad, pad, pad, pad,
-                                cv2.BORDER_CONSTANT, value=border_color)
 
-    t0 = time.time()
     n_ims = 0
     n_ims_nonull = 0
-    dx = int((1. - overlap) * sliceWidth)
-    dy = int((1. - overlap) * sliceHeight)
-    if verbose:
+    dx = int((1. - OVERLAP) * SLICE_WIDTH)
+    dy = int((1. - OVERLAP) * SLICE_HEIGHT)
+    if VERBOSE:
         print('dx', dx)
         print('dy', dy)
-    dict_yolo = {}
-    dict_bbx = {}
-    max_annot = 0
+
 
     for y in range(0, im_h, dy):  # sliceHeight):
         for x in range(0, im_w, dx):  # sliceWidth):
             n_ims += 1
             # extract image
             # make sure we don't go past the edge of the image
-            if y + sliceHeight > im_h:
-                y0 = im_h - sliceHeight
+            if y + SLICE_HEIGHT > im_h:
+                y0 = im_h - SLICE_HEIGHT
             else:
                 y0 = y
-            if x + sliceWidth > im_w:
-                x0 = im_w - sliceWidth
+            if x + SLICE_WIDTH > im_w:
+                x0 = im_w - SLICE_WIDTH
             else:
                 x0 = x
 
-            window_c = resized_img[y0:y0 + sliceHeight, x0:x0 + sliceWidth]
-            if create_mask == True:
-                window_mask = resized_mask[y0:y0 + sliceHeight, x0:x0 + sliceWidth]
-##           gt_c = gt_image[y0:y0 + sliceHeight, x0:x0 + sliceWidth]
-#             gt_c = image[y0:y0 + sliceHeight, x0:x0 + sliceWidth]
+            window_c = resized_img[y0:y0 + SLICE_WIDTH, x0:x0 + SLICE_WIDTH]
             win_h, win_w = window_c.shape[:2]
     
             outname_part = 'slice_' + filename + \
             '_' + str(y0) + '_' + str(x0) + \
             '_' + str(win_h) + '_' + str(win_w) + \
             '_' + str(pad)
-
-            if create_mask == True:
-                outname_mask_part = 'slice_mask_' + filename + \
-                    '_' + str(y0) + '_' + str(x0) + \
-                    '_' + str(win_h) + '_' + str(win_w) + \
-                    '_' + str(pad)
-        
-            # [x0, x1, y0, y1]
-
 
             # get black and white image
             window = cv2.cvtColor(window_c, cv2.COLOR_BGR2GRAY)
@@ -151,78 +127,23 @@ def slice_img(INPUT_FOLDER, RESIZE_FACTOR = 1,
             zero_frac = float(zero_counts) / win_size
             # print ("zero_frac", zero_fra
             # skip if image is mostly empty
-            if zero_frac >= zero_frac_thresh:
-                if verbose:
+            if zero_frac >= ZERO_FRAC_TRESH:
+                if VERBOSE:
                     print("Zero frac too high at:", zero_frac)
                 continue
                 
-            new_box_coords = []
-            yolt_coords = []
-            annot_count = 0
-
-            for box, cat in zip(resized_box_coords, category_num):
-                cut_box = 0
-                percent_in_cut_img = get_intersection_percent(box, [x0, y0, x0 + sliceWidth, y0 + sliceHeight])
-
-                if percent_in_cut_img > 0.75:   
-                    resized_box_cat = np.asarray([box[0] - x0, box[1] - y0, box[2] - x0, box[3] - y0, cat])
-                    new_box_coords.append(resized_box_cat)
-                    annot_count += 1
-                    # Input to convert: image size: (w,h), box: [x0, x1, y0, y1]
-                    #print(box)
-                    yolt_co_i = convert((win_w, win_h), box)
-                    yolt_coords.append(yolt_co_i)
-
-                    
-            if annot_count > max_annot:
-                max_annot = annot_count
-
             #  save
-            outname_im = os.path.join(outdir_im, outname_part + '.png')
-            if create_mask == True:
-                outname_mask = os.path.join(outdir_mask, outname_mask_part + '.png')
-            txt_outpath = os.path.join(outdir_label, outname_part + '.txt')
-
-            dict_yolo[outname_im] = yolt_coords
-            dict_bbx[outname_im] = new_box_coords
-          
-
+            outname_im = os.path.join(OUTPUT_DIR, outname_part + '.png')
+         
             # save yolt ims
-            if verbose:
+            if VERBOSE:
                 print("image output:", outname_im)
             cv2.imwrite(outname_im, window_c)
-            if create_mask == True:
-                cv2.imwrite(outname_mask, window_mask)
 
-            # save yolt labels
-            txt_outfile = open(txt_outpath, "w+")
-            if verbose:
-                print("txt output:" + txt_outpath)
-            for bb in yolt_coords:
-                outstring = str(category_num) + " " + \
-                    " ".join([str(a) for a in bb]) + '\n'
-                if verbose:
-                    print("outstring:", outstring)
-                txt_outfile.write(outstring)
-            txt_outfile.close()
 
-            # if desired, save coords files
-            # save box coords dictionary so that yolt_eval.py can read it
-            if len(box_coords_dir) > 0:
-                coords_dic = gt_dic_from_box_coords(box_coords)
-                outname_pkl = os.path.join(
-                    box_coords_dir, outname_part + '_' + category + '.pkl')
-                pickle.dump(coords_dic, open(outname_pkl, 'wb'), protocol=2)
-            if len(yolt_coords_dir) > 0:
-                outname_pkl = os.path.join(
-                    yolt_coords_dir, outname_part + '_' + category + '.pkl')
-                pickle.dump(yolt_coords, open(outname_pkl, 'wb'), protocol=2)
-
-            n_ims_nonull += 1
-
-    if verbose:
+    if VERBOSE:
         print("Num slices:", n_ims, "Num non-null slices:", n_ims_nonull,
-              "sliceHeight", sliceHeight, "sliceWidth", sliceWidth)
+              "sliceHeight", SLICE_HEIGHT, "sliceWidth", SLICE_WIDTH)
         print("Time to slice", input_im, time.time()-t0, "seconds")
 
     return dict_yolo, dict_bbx, max_annot
